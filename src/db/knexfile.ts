889ec298046eba92a26410a76f9fe09da614b037
Andrew Dawes <andrewjamesdawes@gmail.com>
2023-09-24 18:26:18 +0000

    Get working Knex connection

---
+++ src/db/knexfile.ts
@@ -0,0 +1,55 @@
+import type { Knex } from 'knex';
+
+// Update with your config settings.
+
+const config: { [key: string]: Knex.Config } = {
+    development: {
+        client: 'mysql2',
+        connection: {
+            database: 'example',
+            user: 'example',
+            password: 'example',
+        },
+        pool: {
+            min: 2,
+            max: 10,
+        },
+        migrations: {
+            tableName: 'knex_migrations',
+        },
+    },
+
+    staging: {
+        client: 'postgresql',
+        connection: {
+            database: 'my_db',
+            user: 'username',
+            password: 'password',
+        },
+        pool: {
+            min: 2,
+            max: 10,
+        },
+        migrations: {
+            tableName: 'knex_migrations',
+        },
+    },
+
+    production: {
+        client: 'postgresql',
+        connection: {
+            database: 'my_db',
+            user: 'username',
+            password: 'password',
+        },
+        pool: {
+            min: 2,
+            max: 10,
+        },
+        migrations: {
+            tableName: 'knex_migrations',
+        },
+    },
+};
+
+export default config;
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,23 @@
 import express from 'express';
+import knex from 'knex';
+import knexConfig from './db/knexfile.js';
+import { Model } from 'objection';
+
+export const KNEXION_DEV = knex.knex(knexConfig.development);
+
+// Bind all Models to a knex instance. If you only have one database in
+// your server this is all you have to do. For multi database systems, see
+// the Model.bindKnex() method.
+Model.knex(KNEXION_DEV);
+
 const app = express();
 const PROTOCOL = 'http';
 const PORT = 80;
 const HOST = '0.0.0.0';
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
 app.listen(PORT, HOST, () => {
     console.log(`Running on ${PROTOCOL}://${HOST}:${PORT}`);
 });
