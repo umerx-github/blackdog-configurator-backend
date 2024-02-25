c2254e3fd99fa657e902130fe8fb0645eecbade8
Andrew Dawes <andrewjamesdawes@gmail.com>
2024-02-25 17:51:47 +0000

    Add production start script

--- src/db/knexfile.ts
+++ src/db/knexfile.ts
@@ -1,29 +1,30 @@
 import type { Knex } from 'knex';
 
 // Update with your config settings.
+const typeCast = (
+    field: {
+        type: string;
+        length: number;
+        string: () => string;
+    },
+    next: () => void
+) => {
+    if (field.type === 'TINY' && field.length === 1) {
+        const value = field.string();
+        return value ? value === '1' : null;
+    }
+    return next();
+};
 
 const config: { [key: string]: Knex.Config } = {
     development: {
         client: 'mysql2',
         connection: {
-            host: process.env.DB_HOSTNAME,
-            database: 'example',
-            user: 'example',
-            password: 'example',
-            typeCast: (
-                field: {
-                    type: string;
-                    length: number;
-                    string: () => string;
-                },
-                next: () => void
-            ) => {
-                if (field.type === 'TINY' && field.length === 1) {
-                    const value = field.string();
-                    return value ? value === '1' : null;
-                }
-                return next();
-            },
+            host: process.env.BLACKDOG_CONFIGURATOR_BACKEND_DB_HOSTNAME,
+            database: process.env.BLACKDOG_CONFIGURATOR_BACKEND_DB_NAME,
+            user: process.env.BLACKDOG_CONFIGURATOR_BACKEND_DB_USER,
+            password: process.env.BLACKDOG_CONFIGURATOR_BACKEND_DB_PASSWORD,
+            typeCast: typeCast,
         },
         pool: {
             min: 2,
@@ -35,11 +36,13 @@ const config: { [key: string]: Knex.Config } = {
     },
 
     staging: {
-        client: 'postgresql',
+        client: 'mysql2',
         connection: {
-            database: 'my_db',
-            user: 'username',
-            password: 'password',
+            host: process.env.BLACKDOG_CONFIGURATOR_BACKEND_DB_HOSTNAME,
+            database: process.env.BLACKDOG_CONFIGURATOR_BACKEND_DB_NAME,
+            user: process.env.BLACKDOG_CONFIGURATOR_BACKEND_DB_USER,
+            password: process.env.BLACKDOG_CONFIGURATOR_BACKEND_DB_PASSWORD,
+            typeCast: typeCast,
         },
         pool: {
             min: 2,
@@ -51,11 +54,13 @@ const config: { [key: string]: Knex.Config } = {
     },
 
     production: {
-        client: 'postgresql',
+        client: 'mysql2',
         connection: {
-            database: 'my_db',
-            user: 'username',
-            password: 'password',
+            host: process.env.BLACKDOG_CONFIGURATOR_BACKEND_DB_HOSTNAME,
+            database: process.env.BLACKDOG_CONFIGURATOR_BACKEND_DB_NAME,
+            user: process.env.BLACKDOG_CONFIGURATOR_BACKEND_DB_USER,
+            password: process.env.BLACKDOG_CONFIGURATOR_BACKEND_DB_PASSWORD,
+            typeCast: typeCast,
         },
         pool: {
             min: 2,
